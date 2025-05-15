b03949b346a0b63fdc94dfafeedac6ae133208d0
Cameron <cameron_2000@live.co.uk>
2025-05-15 23:20:14 +0100

    added newly created models to admin.py

--- family_ai_app/admin.py
+++ family_ai_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ticket, Message, Notification
 
 # Register your models here.
+
+admin.site.register(Ticket)
+
+admin.site.register(Message)
+
+admin.site.register(Notification)
