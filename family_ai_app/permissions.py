0259e5e7efeb1596cfa670ac7d1e1881943645f7
Cameron <cameron_2000@live.co.uk>
2025-05-16 18:11:11 +0100

    add IsSenderOrAdmin permission

--- family_ai_app/permissions.py
+++ family_ai_app/permissions.py
@@ -7,3 +7,12 @@ class IsOwnerOrAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff or request.user.is_superuser or obj.user == request.user
+    
+    
+class IsSenderOrAdmin(permissions.BasePermission):
+    """
+    Allows access only to the sender of the message or admin users.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or request.user.is_superuser or obj.sender == request.user
