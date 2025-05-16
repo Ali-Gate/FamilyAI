c5e51fbecb34310242d22b149412981104242471
Cameron <cameron_2000@live.co.uk>
2025-05-16 15:25:54 +0100

    seet debug to false

--- family_ai/settings.py
+++ family_ai/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://family-ai-3bfb51c271b9.herokuapp.com',
                 '.herokuapp.com',
