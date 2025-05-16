dea60e85f96aab210d404cffdc03e3d6ab15a7a1
Cameron <cameron_2000@live.co.uk>
2025-05-16 18:02:50 +0100

    refactor permissions.py

--- family_ai/permissions.py
+++ family_ai/permissions.py
@@ -2,12 +2,8 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     """
-    Custom permission to allow object access to the owner or an admin.
+    Allows access to the owner of the object or an admin user.
     """
 
     def has_object_permission(self, request, view, obj):
-        # Allow access if the user is admin
-        if request.user.is_staff or request.user.is_superuser:
-            return True
-        # Otherwise, allow access only if the user owns the object
-        return obj.user == request.user
+        return request.user.is_staff or request.user.is_superuser or obj.user == request.user
