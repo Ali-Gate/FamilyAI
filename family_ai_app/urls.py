087708b5214ce73158a7dfbe117fa0097719b88b
Ali-Gate <ali_also@yahoo.es>
2025-05-17 11:46:19 +0100

    added login, register and logout files, connecting  nav items to views

--- family_ai_app/urls.py
+++ family_ai_app/urls.py
@@ -9,4 +9,7 @@ app_name = 'family_ai_app'
 # The urlpatterns list routes URLs to views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- family_ai_app/views.py
+++ family_ai_app/views.py
@@ -6,3 +6,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'family_ai/home.html')
+
+def register(request):  
+    return render(request, 'family_ai/register.html')
+
+
+def login(request):
+    return render(request, 'family_ai/login.html')
+
+
+def logout(request):
+    return render(request, 'family_ai/logout.html')
