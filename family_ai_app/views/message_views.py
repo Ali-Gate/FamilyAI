b433ad74e88e23976547be0d2e675f3013a13943
Cameron <cameron_2000@live.co.uk>
2025-05-17 20:24:10 +0100

    add response method to relevent endpoints

--- family_ai_app/views/message_views.py
+++ family_ai_app/views/message_views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from family_ai_app.models import Message
 from family_ai_app.serializers import MessageSerializer
 from family_ai_app.permissions import IsSenderOrAdmin
@@ -14,6 +15,13 @@ class MessageListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 class MessageDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated, IsSenderOrAdmin]
@@ -26,3 +34,16 @@ class MessageDetailView(generics.RetrieveUpdateDestroyAPIView):
         serializer.validated_data.pop('sender', None)
         serializer.save()
 
+    def put(self, request, *args, **kwargs):
+        return self._update_response(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self._update_response(request, partial=True, *args, **kwargs)
+
+    def _update_response(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
