ebc8b9ae0df94bbdd36e99c74bdad61c7ecc21e1
Cameron <cameron_2000@live.co.uk>
2025-05-17 22:41:38 +0100

    wire up ai to notifications

--- family_ai_app/urls.py
+++ family_ai_app/urls.py
@@ -21,7 +21,7 @@ app_name = 'family_ai_app'
 urlpatterns = [
     # Navigation endpoints
     path('', home, name='home'),
-    path('alerts/', alerts, name="alerts"),
+    path('notifications/', alerts, name='alerts'),
 
     # User endpoints
     path('users/', UserListView.as_view(), name='user-list'),
@@ -36,7 +36,6 @@ urlpatterns = [
     path('messages/', MessageListCreateView.as_view(), name='message-list-create'),
     path('messages/<int:pk>/', MessageDetailView.as_view(), name='message-detail'),
 
-    # Notification endpoints
-    path('notifications/', NotificationListView.as_view(), name='notification-list'),
-    path('notifications/<int:pk>/', NotificationDetailView.as_view(), name='notification-detail'),
+    path('api/notifications/', NotificationListView.as_view(), name='notification-list'),
+    path('api/notifications/<int:pk>/', NotificationDetailView.as_view(), name='notification-detail'),
 ]
